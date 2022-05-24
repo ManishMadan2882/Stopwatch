fc9c3a0724f3280781559528afe8990e0873c06a
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2022-05-25 01:55:28 +0530

    Files added
    Update: responsive, precision increased

--- stopwatch/back.js
+++ stopwatch/back.js
@@ -3,20 +3,23 @@ const startBtn = document.querySelector("#start");
 const pauseBtn = document.querySelector("#stop");
 const resetBtn = document.querySelector("#reset");
 //let time=document.getElementById("time");
-
-var hrs=0,secs=0,mins=0;
+var secs=0,mins=0,millis=0
+//var hrs=0,secs=0,mins=0;
 var str="00:00:00";
 var t;
 function start(){//works for the  start button
     t=setInterval(()=>{
+        millis++;
+        if(millis==100)
+        {
         secs++;
+        }
+        millis=millis%100
         if(secs==60)mins++;
         secs=secs%60;
-       if(mins==60)hrs++;
-       mins=mins%60;
-       str=doubleDigit(hrs.toString())+":"+ doubleDigit(mins.toString())+":"+ doubleDigit(secs.toString()); 
+       str=doubleDigit(mins.toString())+":"+ doubleDigit(secs.toString())+"."+ doubleDigit(millis.toString()); 
         timeDisplay.innerHTML=str;
-    },1000)
+    },10)
 }
 function stop(){
     clearInterval(t);
@@ -24,8 +27,8 @@ function stop(){
 }
 function reset(){
     clearInterval(t);
-    str="00:00:00";
-    hrs=0;mins=0;secs=0;
+    str="00:00.00";
+    millis=0;mins=0;secs=0;
     timeDisplay.innerHTML=str;
 }
 var running=false;
